6b4655994654a93a6f5312a58b8a5274d575ae6c
DDDDDelta <2303718160@qq.com>
2023-07-11 19:41:28 +0800

    saving progress

--- board.cpp
+++ board.cpp
@@ -8,22 +8,22 @@ Board::Board() :
 }
 
 void Board::set_up() {
-    this->get_piece({ 1, 1 }) = WhiteRook;
-    this->get_piece({ 2, 1 }) = WhiteKnight;
-    this->get_piece({ 3, 1 }) = WhiteBishop;
-    this->get_piece({ 4, 1 }) = WhiteQueen;
-    this->get_piece({ 5, 1 }) = WhiteKing;
-    this->get_piece({ 6, 1 }) = WhiteBishop;
-    this->get_piece({ 7, 1 }) = WhiteKnight;
-    this->get_piece({ 8, 1 }) = WhiteRook;
-    this->get_piece({ 1, 8 }) = BlackRook;
-    this->get_piece({ 2, 8 }) = BlackKnight;
-    this->get_piece({ 3, 8 }) = BlackBishop;
-    this->get_piece({ 4, 8 }) = BlackQueen;
-    this->get_piece({ 5, 8 }) = BlackKing;
-    this->get_piece({ 6, 8 }) = BlackBishop;
-    this->get_piece({ 7, 8 }) = BlackKnight;
-    this->get_piece({ 8, 8 }) = BlackRook;
+    this->get_piece({ 1, 1 } ) = WhiteRook;
+    this->get_piece({ 2, 1 } ) = WhiteKnight;
+    this->get_piece({ 3, 1 } ) = WhiteBishop;
+    this->get_piece({ 4, 1 } ) = WhiteQueen;
+    this->get_piece({ 5, 1 } ) = WhiteKing;
+    this->get_piece({ 6, 1 } ) = WhiteBishop;
+    this->get_piece({ 7, 1 } ) = WhiteKnight;
+    this->get_piece({ 8, 1 } ) = WhiteRook;
+    this->get_piece({ 1, 8 } ) = BlackRook;
+    this->get_piece({ 2, 8 } ) = BlackKnight;
+    this->get_piece({ 3, 8 } ) = BlackBishop;
+    this->get_piece({ 4, 8 } ) = BlackQueen;
+    this->get_piece({ 5, 8 } ) = BlackKing;
+    this->get_piece({ 6, 8 } ) = BlackBishop;
+    this->get_piece({ 7, 8 } ) = BlackKnight;
+    this->get_piece({ 8, 8 } ) = BlackRook;
 
     for (std::uint8_t i = 1; i <= 8; i++) {
         this->get_piece({ i, 2 }) = WhitePawn;
