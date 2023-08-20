6812088207337f626a6ac299d694c4119f877c38
bobzheng <shenyanzheng00@gmail.com>
2023-08-20 14:26:28 +0800

    Fix board orientation

--- GUI.cpp
+++ GUI.cpp
@@ -6,7 +6,7 @@
 #include "code_utils.inc"
 
 
-#define WHITE_TILE(x, y) (x % 2 == y % 2)
+#define WHITE_TILE(x, y) (x % 2 != y % 2)
 
 NAMESPACE_BOBZHENG00_START
 
@@ -14,23 +14,24 @@ void GUI::render_piece(DDDelta::BoardCoor coor) {
 
     if (_chess_game->get_piece(coor))
     {
-        std::cout<< coor.x << std::endl;
-        std::cout<< coor.y << std::endl;
+//        std::cout<< coor.x << std::endl;
+//        std::cout<< coor.y << std::endl;
 
         SDL_Texture* image_texture =
-                SDL_CreateTextureFromSurface(renderer, _piece_map.at(_chess_game->get_piece(coor).value()).get());
-        SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+                SDL_CreateTextureFromSurface(renderer, _piece_map.at(_chess_game->get_piece(coor.x, coor.y
+                        ).value()).get());
+        SDL_Rect image_rect = {(coor.x-1)*100, (8 - coor.y)*100, 100, 100};
         SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
 
         SDL_DestroyTexture(image_texture);
-        std::cout<< "success" << std::endl;
+//        std::cout<< "success" << std::endl;
     }
 }
 
 
 void GUI::render_move(DDDelta::BoardCoor coor){
     SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, _move_tile.get());
-    SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+    SDL_Rect image_rect = {(coor.x-1)*100, (8-coor.y)*100, 100, 100};
     SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
     SDL_DestroyTexture(image_texture);
 }
@@ -38,14 +39,14 @@ void GUI::render_move(DDDelta::BoardCoor coor){
 
 void GUI::render_capture(DDDelta::BoardCoor coor){
     SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, _capture_tile.get());
-    SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+    SDL_Rect image_rect = {(coor.x-1)*100, (8-coor.y)*100, 100, 100};
     SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
     SDL_DestroyTexture(image_texture);
 }
 
 void GUI::render_select(DDDelta::BoardCoor coor){
     SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, _select_tile.get());
-    SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+    SDL_Rect image_rect = {(coor.x-1)*100, (8-coor.y)*100, 100, 100};
     SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
     SDL_DestroyTexture(image_texture);
 }
@@ -153,7 +154,7 @@ void GUI::render_tile(DDDelta::BoardCoor coor){
     if (_opt_hover)
     {
         SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, _hover_tile.get());
-        SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+        SDL_Rect image_rect = {(coor.x-1)*100, (8-coor.y)*100, 100, 100};
         SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
         _opt_hover.reset();
         SDL_DestroyTexture(image_texture);
@@ -164,8 +165,7 @@ void GUI::render_tile(DDDelta::BoardCoor coor){
     if (WHITE_TILE(coor.x, coor.y))
     {
         SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, _white_tile.get());
-        std::cout << SDL_GetError() << std::endl;
-        SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+        SDL_Rect image_rect = {(coor.x-1)*100, (8-coor.y)*100, 100, 100};
         SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
         SDL_DestroyTexture(image_texture);
 //        return;
@@ -173,7 +173,7 @@ void GUI::render_tile(DDDelta::BoardCoor coor){
     else
     {
         SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, _black_tile.get());
-        SDL_Rect image_rect = {(coor.x-1)*100, (coor.y-1)*100, 100, 100};
+        SDL_Rect image_rect = {(coor.x-1)*100, (8-coor.y)*100, 100, 100};
         SDL_RenderCopy(renderer, image_texture, nullptr, &image_rect);
         SDL_DestroyTexture(image_texture);
 //        return;
@@ -193,7 +193,6 @@ void GUI::render_tile(DDDelta::BoardCoor coor){
 void GUI::player_init() {
     SDL_Color textColor = {255, 255, 255, 255};
     SDL_Surface *surface = TTF_RenderText_Solid(TTF_OpenFont("../font.ttf", 24), _chess_game->player_white.name[0].c_str(), textColor);
-    std::cout << SDL_GetError() << std::endl;
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_Rect textRect = {850, 50, surface->w, surface->h};
     SDL_RenderCopy(renderer, texture, nullptr, &textRect);
@@ -210,12 +209,11 @@ void GUI::player_init() {
 void GUI::board_init() {
 
     player_init();
-    std::cout << SDL_GetError() << std::endl;
     for (std::int8_t row = 1; row <= 8; row++) {
         for (std::int8_t col = 1; col <= 8; col++) {
             render_tile(DDDelta::BoardCoor {col, row});
             render_piece(DDDelta::BoardCoor {col, row});
-            std::cout << SDL_GetError() << std::endl;
+
         }
     }
 
--- controller.cpp
+++ controller.cpp
@@ -13,8 +13,10 @@
 
 #define ON_BOARD(x, y) (x < 800 && y < 800)
 #define NEED_REDRAW_PREVIOUS_TILE(mouse_x, mouse_y, board_x, board_y) \
-(board_x != 0 && board_y != 0 && ((mouse_x / 100 + 1) != board_x || (mouse_y / 100 + 1) != board_y))
+(board_x != 0 && board_y != 0 && ((mouse_x / 100 + 1) != board_x || (8 - mouse_y / 100) != board_y))
 #define TO_BOARDCOOR(x, y) (DDDelta::BoardCoor(x / 100 + 1, y / 100 + 1))
+#define TO_CHESSBOARDCOOR(x, y) (DDDelta::BoardCoor(x / 100 + 1, 8 - y / 100))
+#define CHESSBOARDCOOR_TO_BOARDCOOR(coor) (DDDelta::BoardCoor(coor.x, 9 - coor.y))
 // #define ABLE_TO_SELECT(x, y) (!_chess_game->get_selection())
 // #define UNABLE_TO_EXECUTE(x, y) (!_chess_game->execute_move(TO_BOARDCOOR(x, y)))
 // #define NOT_UNIQUE_ACTION(x, y) (_chess_game->execute_move(TO_BOARDCOOR(x, y)).value() == DDDelta::E_UniqueAction::None)
@@ -50,7 +52,7 @@ void Controller::_handle_mouse_move() {
     }
 
     // Update current tile
-    opt_hover = TO_BOARDCOOR(mouse_x, mouse_y);
+    opt_hover = TO_CHESSBOARDCOOR(mouse_x, mouse_y);
 
     // Draw hovered tile and piece
     _gui->set_hover(opt_hover.value());
@@ -76,7 +78,7 @@ void Controller::_handle_mouse_click() {
         if (!_opt_selected) //TODO !chess_game->get_selection()
         {
             std::cout << "selected one piece" << std::endl;
-            _wp_moves = _chess_game->select_piece(TO_BOARDCOOR(mouse_x, mouse_y));
+            _wp_moves = _chess_game->select_piece(TO_CHESSBOARDCOOR(mouse_x, mouse_y));
             _opt_selected = _chess_game->get_selection();
 
             if (_wp_moves.lock() == nullptr) {
@@ -115,11 +117,13 @@ void Controller::_handle_mouse_click() {
             try
             {
                 std::cout << "check if unique" << std::endl;
-                action = _chess_game->execute_move(TO_BOARDCOOR(mouse_x, mouse_y));
-                std::cout << "finish execute" << std::endl;
+                action = _chess_game->execute_move(TO_CHESSBOARDCOOR(mouse_x, mouse_y));
+                std::cout << mouse_x/100+1 << std::endl;
+                std::cout << 8 - mouse_y / 100 << std::endl;
             }
             catch(DDDelta::throwable::pawn_promote& e)
             {
+                std::cout << "promote" << std::endl;
                 _gui->render_promote_selection(TO_BOARDCOOR(mouse_x, mouse_y));
 
                 SDL_Event promote;
@@ -134,6 +138,7 @@ void Controller::_handle_mouse_click() {
 
             if (!action)
             {
+                std::cout << "regular action" << std::endl;
                 _gui->render_tile(_opt_selected.value());
                 _gui->render_piece(_opt_selected.value());
 
@@ -151,6 +156,7 @@ void Controller::_handle_mouse_click() {
 
                 _wp_moves.reset();
                 _opt_selected.reset();
+                SDL_RenderPresent(_gui->renderer);
                 return;
             }
 
@@ -174,8 +180,10 @@ void Controller::_handle_mouse_click() {
                     _gui->render_piece(pm.coor);
                 }
 
+                std::cout << "regular action finish" << std::endl;
                 _wp_moves.reset();
                 _opt_selected.reset();
+                SDL_RenderPresent(_gui->renderer);
                 return;
             }
 
