c9074a805205c4ace89f21ba4caab663ab3029ec
Steven(Chenwei) Shi <76062592+DDDDDelta@users.noreply.github.com>
2023-09-20 20:26:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "controller.h"
 
 int main(int argc, char* argv[]) {
-    DDDelta::ChessGame chess_game { DDDelta::Player { "Bob", "Shit", "Zheng" , 1, "liangtuoshi"}, DDDelta::Player { "Steven", "DDDelta", "Shi", 1, "yituoshi"} };
+    DDDelta::ChessGame chess_game { DDDelta::Player { "Bob", "BobZheng00", "Zheng" , 2000, "Dev" }, DDDelta::Player { "Steven", "DDDelta", "Shi", 2300, "GM" } };
     BobZheng00::GUI GUI = BobZheng00::GUI(&chess_game);
     BobZheng00::Controller controller = BobZheng00::Controller(&GUI, &chess_game);
     controller.run();
